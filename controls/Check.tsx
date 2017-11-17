f4f63035c7b059626ec650b68bac1f4335f8c30a
Judy <850832627@qq.com>
2017-11-17 22:43:40 +0800

    Update Check.tsx

--- controls/Check.tsx
+++ controls/Check.tsx
@@ -2,5 +2,12 @@ class Check extends React.Component<Props & WithStyles>{
   constructor(props: Props&WithStyles){
     super(props);
     }
-    
-  }
+   
+  render(){
+		let {name, label, classes, disabled, value} = this.props;
+		let input = [
+			<input key="1" type="checkbox" name={name} disabled={disabled} onChange={this.handleClick} defaultChecked={value}/>
+			<span key="2" className={cs(classes.indicator,{checked:value})}/>
+		]
+}
+																		
