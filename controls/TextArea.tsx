a0876bf3fba33effa5a3a931978c1b9352cf2a55
Judy <850832627@qq.com>
2018-01-10 14:10:48 +0800

    使用公共样式

--- controls/TextArea.tsx
+++ controls/TextArea.tsx
@@ -1,34 +1,44 @@
+import * as cs from "classnames"
 import * as React from "react"
 import { connect } from "utils"
+import { label as labelStyle, ctrl as ctrlStyle } from "./styles"
 
 const styles = {
   textArea: {
-    backgroundColor: "#f7f7f7",
-    border: "none",
+    ...ctrlStyle,
+    padding: 10,
+    height: 140,
     resize: "none",
   },
+  label: labelStyle,
 }
 
 interface Props {
   name: string
-  rows: number
-  cols: number
   placeholder: string
   onChange?: (event: React.ChangeEvent<HTMLTextAreaElement>) => void
   classes?: Classes
+  label?: string
+  className?: string
 }
 
-function TextArea({name, rows, cols, onChange, classes, placeholder}: Props) {
-  return (
-    <textarea
-      name={name}
-      rows={rows}
-      cols={cols}
-      placeholder={placeholder}
-      onChange={onChange}
-      className={classes.textArea}
-    />
-  )
+function TextArea({name, onChange, classes, className, placeholder, label}: Props) {
+  const textArea =
+  (<textarea
+        name={name}
+        placeholder={placeholder}
+        onChange={onChange}
+        className={classes.textArea}
+  />
+ )
+  return label ?
+   <label className={cs(className)}>
+     <div className={classes.label}>
+       {label}
+     </div>
+       {textArea}
+   </label>
+  : textArea
 }
 
 export default connect(TextArea, {styles})
