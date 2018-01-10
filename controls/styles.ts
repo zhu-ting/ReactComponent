b631636e0e9d94cbf85a00d20bd4c21d3defe4b6
Judy <850832627@qq.com>
2018-01-10 15:10:09 +0800

    Create styles.ts

---
+++ controls/styles.ts
@@ -0,0 +1,28 @@
+// 每一个输入框的提示文字
+export const label = {
+  height: 19,
+  lineHeight: "19px",
+  fontSize: 16,
+  color: "#89949B",
+  marginBottom: 6,
+}
+
+export const ctrl = {
+  boxSizing: "border-box",
+  border: "1px solid #DDDDDD",
+  height: 36,
+  width: "100%",
+  verticalAlign: "middle",
+  lineHeight: "36px",
+  color: "#233641",
+  fontSize: 16,
+  fontWeight: 400,
+  appearance: "none",
+  padding: "0 10px",
+  backgroundColor: "#fff",
+}
+// 不可编辑状态的样式
+export const staticStyle = {
+  background: "#f7f7f7",
+  border: "none",
+}
