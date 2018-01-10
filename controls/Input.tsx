9e7db6e8108d2297b7ed7958275e4db4785dc4ee
Judy <850832627@qq.com>
2018-01-10 11:18:15 +0800

    增加label 部分

--- controls/Input.tsx
+++ controls/Input.tsx
@@ -4,45 +4,53 @@ import * as cs from "classnames"
 
 const styles = {
   input: {
-    outline: 'none',
-    border: 'none',
-    borderRadius: 3,
-    boxShadow: 'inset 0 0 0 1px rgba(16,22,26,0.15), inset 0 1px 1px 0 rgba(16,22,26,0.20);',
+    border: "2px solid #dddddd",
     background: '#fff',
     height: 40,
     padding: '0 10px',
     verticalAlign: 'middle',
-    lineHeight: "normal,
+    lineHeight: "normal",
     color: '#182026',
     fontSize: 16,
     fontWeight: 400,
-    transition: 'box-shadow 100ms cubic-bezier(0.4, 1, 0.75, 0.9)',
     appearance: 'none',
     "&::placeholder": {
       color: 'rgba(92,113,128,0.5)'
     }
-  }
+  },
+  label:{
+    height: 18,
+    lineHeight: "18px",
+    fontSize: 15,
+    color: "#89949a",
+    marginBottom: 10,
+  },
 }
 
 interface Props {
   type: string;
   name: string;
   placeholder: string;
-  onChange: (event: React.ChangeEvent<HTMLInputElement>)=> void;
+  value?: string
+  onChange: (name: string, value: string)=> void;
   className?: string
+  label?: string
 }
 
-const Input: React.SFC<Props&WithStyles> = ({type, name, onChange, placeholder, className, disabled}) => {
-  return (
+const Input: React.SFC<Props&WithStyles> = ({type, name, onChange, classes, placeholder, className, disabled, label, value}) => {
+  let changeHandler = onChange?({target:{name,value}}: React.changeEvent<HTMLInputElement>)=>onChange(value,name):null
+  const input = (
     <input
       type={type}
+      value={value}
       name={name}
       placeholder={placeholder}
-      onChange={onChange}
-      className={cs(classes.input, className)}
+      onChange={changeHandler}
+      className={cs(classes.input, {[className]: !label})}
       disabled={disabled}
     />
   )
+  return label?<label className={cs(className)}><div className={classes.label}>{label}</div>{input}</label>:input
  }
                        
 Input.defaultProps = {
